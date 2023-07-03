70680b9f7f64fa7ee8ece4f06377a8daa0274d5f
rkdalswl718 <rkdalswl070718@gmail.com>
2023-07-03 23:25:05 +0900

    ADD :: script

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const cat1 = document.querySelector('.cat1');
+const scoreDisplay = document.querySelector('.score');
 
-let score = document.querySelector('.score');
+let score = 0;
 
-const changeCat(cat) = () => {
-    cat.src = 
-
+const changeCat = () => {
+    cat1.src = "https://i.pinimg.com/564x/b4/5f/6a/b45f6a22a04be2f1022ff81808590cc2.jpg";
+    score++;
+    scoreDisplay.textContent = score;
 }
+
+cat1.addEventListener("click", changeCat);
