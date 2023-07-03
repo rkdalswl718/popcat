16e0a6f87ec37313dfde44a27ac3602aaefe901b
rkdalswl718 <rkdalswl070718@gmail.com>
2023-07-04 00:26:04 +0900

    FIX :: 눌렀을 떄만 이미지 변경

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const cat1 = document.querySelector('.cat1');
-const scoreDisplay = document.querySelector('.score');
+const cat1 = document.querySelector(".cat1");
+const scoreDisplay = document.querySelector(".score");
 
 let score = 0;
+let isClicked = false; // 클릭 상태를 저장하는 변수
+let previousSrc = cat1.src;
 
 const changeCat = () => {
-    cat1.src = "https://i.pinimg.com/564x/b4/5f/6a/b45f6a22a04be2f1022ff81808590cc2.jpg";
-    score++;
-    scoreDisplay.textContent = score;
-}
+    if (!isClicked) {
+        isClicked = true; // 클릭 상태로 변경
+        cat1.src ="https://i.pinimg.com/564x/b4/5f/6a/b45f6a22a04be2f1022ff81808590cc2.jpg";
+        score++;
+        scoreDisplay.textContent = score;
+    }
+};
 
-cat1.addEventListener("click", changeCat);
+const restoreCat = () => {
+    if (isClicked) {
+        cat1.src = previousSrc; // 이전 이미지로 변경
+        isClicked = false; // 클릭 상태 해제
+    }
+};
+
+cat1.addEventListener("mousedown", changeCat);
+cat1.addEventListener("mouseup", restoreCat);
