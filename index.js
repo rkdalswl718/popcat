3d896f93f4fd9389cf8d9f36d01ef7d87fe0cb9c
rkdalswl718 <rkdalswl070718@gmail.com>
2023-07-04 15:05:41 +0900

    ADD :: 로컬스토리지

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 const cat1 = document.querySelector(".cat1");
 const scoreDisplay = document.querySelector(".score");
 
-let score = localStorage.getItem("score",) ;
+let score; // 로컬 스토리지에서 점수 불러오기
 let isClicked = false; // 클릭 상태를 저장하는 변수
 let previousSrc = cat1.src;
 
 const changeCat = () => {
     if (!isClicked) {
         isClicked = true; // 클릭 상태로 변경
-        cat1.src ="https://i.pinimg.com/564x/b4/5f/6a/b45f6a22a04be2f1022ff81808590cc2.jpg";
+        cat1.src = "https://i.pinimg.com/564x/b4/5f/6a/b45f6a22a04be2f1022ff81808590cc2.jpg";
         score++;
-        scoreDisplay.textContent = score;
         
         const audio = new Audio('./bbyk.mp3');
         audio.play();
+        
+        // 로컬 스토리지에 점수 저장
+        localStorage.setItem("score", score);
+        scoreDisplay.textContent = score;
     }
-};  
+};
+
+
+window.addEventListener('load',() => {
+    score = localStorage.getItem('score');
+    scoreDisplay.textContent = score;
+})
 
 const restoreCat = () => {
     if (isClicked) {
@@ -25,6 +34,6 @@ const restoreCat = () => {
 };
 
 cat1.addEventListener("mousedown", changeCat);
-document.addEventListener("keydown",changeCat);
+document.addEventListener("keydown", changeCat);
 cat1.addEventListener("mouseup", restoreCat);
-document.addEventListener("keyup",restoreCat);
+document.addEventListener("keyup", restoreCat);
